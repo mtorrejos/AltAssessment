4c2d0cb8d9b925973f774e91b6167ba86d9640fd
mtorrejos <106376979+mtorrejos@users.noreply.github.com>
2022-05-30 18:24:32 +0800

    Add files via upload

--- E-Guard/src/com/gui/Dashboard.java
+++ E-Guard/src/com/gui/Dashboard.java
@@ -1,9 +1,11 @@
 package com.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class Dashboard extends Login{
 
-	public Dashboard() {
+	public Dashboard(String[] t, String[] u) {
 		getContentPane().removeAll();
 		setBackground(new Color(255, 244, 228));
 		setBounds(100, 100, 375, 812);
@@ -25,41 +27,50 @@ public class Dashboard extends Login{
 			File file = new File(pathIndData);	
 			String line = "";
 			String[] columns = {"DATE","TIME","PLACE","TYPE","TEMP","# of PEOPLE"};
-			String[] data;
-
-			JTable table = new JTable(new DefaultTableModel(null, columns));		
-			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			String[][] data = new String[30][6];
+			int i = 0;
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				while ((line = br.readLine()) != null) {
-					data = line.split(",");
-					model.addRow(data);	
+					data[i] = line.split(",");
+					System.out.print(file);
+					i++;
+					
 				}
+				
 			}
 			
-			catch(Exception err) {
+			catch(IOException err) {
 				err.getStackTrace();
+				System.out.print(file);
 			}
 			
+			JTable table = new JTable(data, columns);
+			table.setBounds(10, 33, 339, 751);
+		    add(table);
+		    
 			JLabel lblNewLabel = new JLabel("Number of Visitors During Time Interval:");
 			lblNewLabel.setBounds(20, 724, 273, 14);
 			getContentPane().add(lblNewLabel);
+			
+			JLabel lblNewLabel_1 = new JLabel("0");
+			lblNewLabel_1.setBounds(303, 724, 46, 14);
+			getContentPane().add(lblNewLabel_1);
 		}
 		
 		else {
 			File file = new File(pathBusData);	
 			String line = "";
-			String[] columns = {"DATE","TYPE","CONTACT PERSON","ADDRESS","CONTACT #","TEMP"};
-			String[] data;
-
-			JTable table = new JTable(new DefaultTableModel(null, columns));		
-			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			String[] columns = {"DATE","TIME","PLACE","TYPE","TEMP","# of PEOPLE"};
+			String[][] data = {};
+			int i = 0;
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(file));
 				while ((line = br.readLine()) != null) {
-					data = line.split(",");
-					model.addRow(data);	
+					data[i] = line.split(",");
+					i++;
 				}
+				
 			}
 			
 			catch(Exception err) {
@@ -71,8 +82,6 @@ public class Dashboard extends Login{
 		btnNewButton.setBounds(10, 11, 89, 23);
 		add(btnNewButton);
 		
-		JLabel lblNewLabel_1 = new JLabel("0");
-		lblNewLabel_1.setBounds(303, 724, 46, 14);
-		getContentPane().add(lblNewLabel_1);
+
 	}
 }
