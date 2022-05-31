358ee8cca55536016f24112d58485f901e9fa7ea
mtorrejos <106376979+mtorrejos@users.noreply.github.com>
2022-05-31 21:52:08 +0800

    Add files via upload

--- E-Guard/src/com/gui/Homepage.java
+++ E-Guard/src/com/gui/Homepage.java
@@ -1,6 +1,5 @@
 package com.gui;
 import com.det.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,52 +15,76 @@ import javax.swing.*;
 
 public class Homepage extends Login{
 	
+	String[] t;
+	String[] u;
+	
 	public Homepage(String[] user, String[] time) {	//l: username, password, id; id, login date, login time, logout time
 		getContentPane().removeAll();
-		setBackground(new Color(255, 244, 228));
-		setLayout(null);
-		setBounds(100, 100, 375, 812);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new Color(255, 244, 228));
+        setBounds(100, 100, 365, 655);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        
+        JLabel label = new JLabel ("E-Guard");
+		label.setBounds(130,38,200,50);
+		label.setFont(new Font(null,Font.BOLD,30));
+		label.setForeground(Color.black);
+		add(label);
+		
+		 JLabel label2 = new JLabel ("Online Tracer");
+		 label2.setBounds(138,60,200,50);
+		 label2.setFont(new Font(null,Font.BOLD,15));
+		 label2.setForeground(Color.black);
+		 add(label2);
+		
+		ImageIcon pic = new ImageIcon("medeguard.png"); //medwelcome picture
+		JLabel Picture = new JLabel(pic);
+		add(Picture);
+		Picture.setBounds(63,32,80,80);
 		
 		JLabel lblCode = new JLabel("UNIQUE CODE");
 		lblCode.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCode.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblCode.setBounds(55, 351, 236, 32);
+		lblCode.setForeground(Color.black);
+		lblCode.setBounds(55, 151, 236, 32);
 		add(lblCode);
 		
 		JLabel lblActualCode = new JLabel();
 		lblActualCode.setText(user[2]);
 		lblActualCode.setHorizontalAlignment(SwingConstants.CENTER);
-		lblActualCode.setFont(new Font("Segoe UI Semibold", Font.BOLD, 47));
-		lblActualCode.setBounds(55, 394, 236, 69);
+		lblActualCode.setFont(new Font("Segoe UI Semibold", Font.BOLD, 50));
+		lblActualCode.setForeground(Color.black);
+		lblActualCode.setBounds(30, 210, 300, 69);
 		add(lblActualCode);
 		
 		JButton btnAddtoDb = new JButton("Add to Dashboard");
-		btnAddtoDb.setBounds(94, 474, 158, 41);
+		btnAddtoDb.setBounds(94, 375, 158, 41);
 		setBtnProps(btnAddtoDb);
 		if(user[2].charAt(0) == 'B')
 			add(btnAddtoDb);
 		
 		JButton btnDashboard = new JButton("Dashboard");
-		btnDashboard.setBounds(118, 526, 112, 41);
+		btnDashboard.setBounds(94, 420, 158, 41);
 		setBtnProps(btnDashboard);
 		btnDashboard.addActionListener(dashboardListener);
 		add(btnDashboard);
 		
 		JButton btnProfile = new JButton("Profile");
-		btnProfile.setBounds(118, 578, 112, 41);
+		btnProfile.setBounds(94, 465, 158, 41);
 		setBtnProps(btnProfile);
+		btnProfile.addActionListener(profileListener);
 		add(btnProfile);
 		
 		JButton btnLogout = new JButton("Log-out");
-		btnLogout.setBounds(118, 630, 112, 41);
+		btnLogout.setBounds(94, 510, 158, 41);
 		setBtnProps(btnLogout);
 		btnLogout.addActionListener(backListener);
 		add(btnLogout);
 		
 		setVisible(true);
+		u = user;
+		t = time;
 		
 	}
 	
@@ -69,9 +92,19 @@ public class Homepage extends Login{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			dispose();
-			Dashboard dashboard = new Dashboard();
+			Dashboard dashboard = new Dashboard(u,t);
 		}
 	};
+	
+	
+	ActionListener profileListener =new ActionListener () {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			dispose();
+			Profile profile = new Profile();
+		}
+	};
+	
 	
 	ActionListener logoutListener(String[] user, String[] time) {
 		String[] l = {"","",""};
